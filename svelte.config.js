e89c1dbb5476db641f7224f885a421aceaade086
Chanin Namchit <lodydigital@gmail.com>
2025-11-02 16:23:06 +0700

    update: fix LIFF webhook & config

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,12 @@
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    // 
     adapter: adapter({
-      // ใช้ Netlify Edge Functions เพื่อให้ API ตอบสนองเร็วที่สุด
-      edge: true
+      // ❌ อย่าใช้ edge สำหรับ LINE webhook
+      edge: false
     })
   }
 };
